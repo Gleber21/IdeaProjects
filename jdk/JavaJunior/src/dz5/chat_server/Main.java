dd31ceb7d56b86e013e486a2ef80d91d69cf9da1
Gleber21 <163034411+Gleber21@users.noreply.github.com.>
2024-12-02 17:15:21 +0300

    JavaJunior homework seminar #5

---
+++ jdk/JavaJunior/src/dz5/chat_server/Main.java
@@ -0,0 +1,21 @@
+package dz5.chat_server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.UnknownHostException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(33000);
+            Server server = new Server(serverSocket);
+            server.runServer();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
