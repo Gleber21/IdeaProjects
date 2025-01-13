e9df8c0d346bef3c5be40162427475d384ad3306
Gleber21 <163034411+Gleber21@users.noreply.github.com.>
2025-01-13 22:21:41 +0300

    dz4

---
+++ fraimwork/src/dz4/model/Timesheet.java
@@ -0,0 +1,13 @@
+package dz4.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Timesheet {
+    private Long id;
+    private String project;
+    private int minutes;
+    private LocalDate createdAt;
+}
---
+++ fraimwork/src/dz4/service/ProjectPageService.java
@@ -0,0 +1,36 @@
+package dz4.service;
+
+import lombok.RequiredArgsConstructor;
+import dz4.controller.ProjectPageDto;
+import dz4.model.Project;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProjectPageService {
+
+    private final ProjectService projectService;
+
+    public List<ProjectPageDto> findAllProjects() {
+        return projectService.getAllProjects()
+                .stream()
+                .map(this::convert).toList();
+    }
+
+    public Optional<ProjectPageDto> findProjectById(Long id) {
+        return projectService.getProjectById(id)
+                .map(this::convert);
+    }
+
+    private ProjectPageDto convert(Project project) {
+        ProjectPageDto projectPageDto = new ProjectPageDto();
+
+        projectPageDto.setId(String.valueOf(project.getId()));
+        projectPageDto.setName(project.getProjectName());
+
+        return projectPageDto;
+    }
+}
